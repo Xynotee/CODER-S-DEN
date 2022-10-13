684c5197c8a5bdad14154cb21211eff61bec9067
Oluwatobiloba Okusaga <tobbieokusaga@gmail.com>
2022-10-13 17:48:34 +0100

    week1-completed

--- WEEK-1/variable.js
+++ WEEK-1/variable.js
@@ -4,3 +4,17 @@ let graduate = true
 let anything
 let funds = null
 console.log(firstName, graduate, anything, funds)
+//  variables without assigning values
+let car
+let house
+let school
+let work
+console.log(car, house, school, work)
+// variables to store firstName,lastName... in a single line
+let lastName = 'Okusaga', maritalStatus = 'single', country = 'Nigeria', age = 29
+console.log(firstName, lastName, maritalStatus, country, age)
+// variable myAge and yourAge
+let myAge = 29
+let yourAge = 30
+console.log('I am ' + myAge + ' years old')
+console.log('You are ' + yourAge + ' years old')
