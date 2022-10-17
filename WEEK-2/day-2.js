cc9782b87aed48d50de62e2885f3d633a49da9bd
Oluwatobiloba Okusaga <tobbieokusaga@gmail.com>
2022-10-17 18:05:17 +0100

    wk-3

--- WEEK-2/day-2.js
+++ WEEK-2/day-2.js
@@ -66,13 +66,18 @@ console.log("\"Love is not patronizing and charity isn't about pity, it is about
 const num = '10'
 console.log(parseInt(num) === 10)
 // Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
-
+console.log(parseFloat(num) === 10)
 // Check if 'on' is found in both python and jargon
-
+let a = 'python'
+let b = 'jargon'
+console.log(a.includes('on') && b.includes('on'))
 // I hope this course is not full of jargon. Check if jargon is in the sentence.
-
+let sen = 'I hope this course is not full of jargon.'
+console.log(sen.includes('on'))
 // Generate a random number between 0 and 100 inclusively.
-
+const randNum = math.random()
+let randNumLess = math.ceil(randNum * 100)
+console.log(randNumLess)
 // Generate a random number between 50 and 100 inclusively.
 
 // Generate a random number between 0 and 255 inclusively.
@@ -86,14 +91,17 @@ console.log(parseInt(num) === 10)
 // 3 1 3 9 27
 // 4 1 4 16 64
 // 5 1 5 25 125
+let series = parseInt('1 1 1 1 \n 2 1 2 4 8 \n 3 1 3 9 27')
+console.log(series)
 // Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
-
+let sent = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sent.substring('because').slice())
 // Exercises: Level 3
 // 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
-
+let loveTalk = "'Love is the best thing in this world. Some found their love and some are still looking for their love.'"
+console.log(loveTalk.match('love'))
 // Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
 // Clean the following text and find the most frequent word (hint, use replace and regular expressions).
-
 //     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 // Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
